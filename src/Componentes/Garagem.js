9e809d6ef35e7688e0aca5fec69b827134fcef78
guirozmesquita@gmail.com <guilhermesquita>
2022-10-13 10:38:55 -0400

    template ok

--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -4,10 +4,10 @@ function Garagem() {
   return (
     <div>
       <h1>Garagem de Indio</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <Carro nome='Gol' cor='azul' ano = '2006' flex='não'/>
+      <Carro nome='fusca' cor='amarelo' ano = '1999' flex='não'/>
+      <Carro nome='Kombi' cor='branco' ano = '2010' flex='não'/>
+      <Carro nome='Fit' cor='prata' ano = '2019' flex='sim'/>
     </div>
   );
 }
