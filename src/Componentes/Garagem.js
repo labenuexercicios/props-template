0bab107b090267d45b5e49849481247224ede227
Celeta <marceloceleta@gmail.com>
2023-01-18 11:13:00 -0300

    alterações

--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,13 +1,19 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem(props) {
   return (
     <div>
-      <h1>Garagem da Labenu</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem da {props.nome}</h1>
+      <button onClick={()=> props.apresentaGaragem(props.nome)}>Apresentar a garagem</button>
+      <Carro carro = {props.carro1}/>
+      <Carro carro = {props.carro2}/>
+      {/* <h1>Garagem da {props.nome}</h1>
+      <button onClick={()=> props.apresentaGaragem(props.nome)}>Apresentar a garagem</button>
+      <Carro carro = {props.carro3}/>
+      <Carro carro = {props.carro4}/>
+      <Carro carro = {props.carro5}/>
+      <Carro carro = {props.carro6}/> */}
+      
     </div>
   );
 }
